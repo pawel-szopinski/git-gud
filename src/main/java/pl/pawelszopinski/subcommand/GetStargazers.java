d51305871c8b5c8324f9e578fbf3d3e4a6ba6a2e
pawel-szopinski <szopinski.p@gmail.com>
2020-02-25 22:53:47 +0100

    loop stargazers pages

--- src/main/java/pl/pawelszopinski/subcommand/GetStargazers.java
+++ src/main/java/pl/pawelszopinski/subcommand/GetStargazers.java
@@ -14,6 +14,9 @@ import pl.pawelszopinski.view.ConsoleDisplay;
 import pl.pawelszopinski.view.Displayable;
 
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.Callable;
 
 @Command(name = "stargazers", description = "Print users starring a given repository.")
@@ -50,11 +53,27 @@ public class GetStargazers implements Callable<Integer> {
 
         User[] userArray = gson.fromJson(response.body(), User[].class);
 
-        for (User user : userArray) {
+        List<User> userList = new ArrayList<>(Arrays.asList(userArray));
+
+        int i = 2;
+        while (linkHeader != null && !linkHeader.endsWith("rel=\"first\"")) {
+            uri = "repos/" + owner.getOwner() + "/" +
+                    repository.getRepository() + "/stargazers?per_page=100&page=" + i;
+
+            response = requestProc.sendGet(uri, HttpResponse.BodyHandlers.ofString(), auth.isAuth());
+
+            linkHeader = response.headers().firstValue("link").orElse(null);
+
+            userList.addAll(Arrays.asList(gson.fromJson(response.body(), User[].class)));
+
+            i++;
+        }
+
+        for (User user : userList) {
             System.out.println(user);
         }
 
-        displayMethod.showJson(response);
+//        displayMethod.showJson(response);
 
         return 0;
     }
